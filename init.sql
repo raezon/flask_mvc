a9f6881d458dc3a7ac2e1dd8ec567ba42881bf25
raezon <amardjebabla10@gmail.com>
2025-02-01 21:45:55 +0100

    Enhance docker-compose

--- init.sql
+++ init.sql
@@ -1,4 +1,10 @@
+-- For the root user connecting from localhost:
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+
+-- Create or update the remote root user (if it doesn't exist, create it)
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+
+-- Grant full privileges to remote root user:
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
