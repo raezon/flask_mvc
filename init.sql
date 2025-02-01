0def194c8c4ffe37569a6a80d147f9f5c4be6d2f
raezon <amardjebabla10@gmail.com>
2025-02-01 21:27:59 +0100

    Enhance docker to adapt on vm

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
